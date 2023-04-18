70ee956989d980d66b28e77b4a05e1fd89d59295
Cindi Nichols <cindi.l.nichols@gmail.com>
2023-04-17 18:48:13 -0600

    wk01-prove parts 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userGradeInput = Console.ReadLine();
+
+        int userGrade = int.Parse(userGradeInput);
+        string letterGrade;
+        string letterGradeAddon;
+
+        //determine letter grade
+        if (userGrade >= 90)
+        {
+           letterGrade = "A";
+        }
+        else if (userGrade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (userGrade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (userGrade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else 
+        {
+            letterGrade = "F";
+        }
+
+        //stretch goal - add + or - to printed grade
+         if (userGrade >= 93 || userGrade <= 59)
+        {
+            letterGradeAddon = "";
+        }
+        else if ( userGrade % 10 >= 7) 
+        {
+            letterGradeAddon = "+";
+        } 
+        else if (userGrade % 10 <= 3)
+        {
+            letterGradeAddon = "-";
+        }
+        else
+        {
+            letterGradeAddon = "";
+        }
+
+        //print grade
+        Console.WriteLine($"Your grade is: {letterGrade}{letterGradeAddon}");
+
+        //determine if grade is passing
+        if (userGrade >= 70) 
+        {
+            Console.WriteLine("Congratulations! You Passed!");
+        }
+        else 
+        {
+            Console.WriteLine("You will need to repeat the class. You got this!");
+        }
     }
 }
