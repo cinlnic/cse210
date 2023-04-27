0ae92d23fb648a1d51e9378c9b92beb3fcdb4ca1
Cindi Nichols <cindi.l.nichols@gmail.com>
2023-04-27 11:16:23 -0600

    Wk3 prepare activity

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+public class Job
+{
+   public string company;
+   public string jobTitle;
+   public int startYear;
+   public int endYear;
+
+   public void Display() 
+   {
+      //Console.WriteLine(jobTitle + " (" + company + ") " + startYear + "-" + endYear);
+      Console.WriteLine($"{jobTitle} ({company}) {startYear}-{endYear}");
+   }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1.company = "Microsoft";
+        job1.jobTitle = "Sotware Engineer";
+        job1.startYear = 2012;
+        job1.endYear = 2018;
+
+        Job job2 = new Job();
+        job2.company = "Micron";
+        job2.jobTitle = "Web Developer";
+        job2.startYear = 2018;
+        job2.endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume.name = "Cindi Nichols";
+        myResume.jobs.Add(job1);
+        myResume.jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
