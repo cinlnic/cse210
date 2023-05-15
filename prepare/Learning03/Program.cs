61b5288602788d25f774cf145dbaae92709d00e1
Cindi Nichols <cindi.l.nichols@gmail.com>
2023-05-15 14:27:55 -0600

    Learning03 - fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,23 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
-    }
+   static void Main(string[] args)
+   {
+      Fraction one = new Fraction();
+      Console.WriteLine(one.GetFractionString());
+      Console.WriteLine(one.GetDecimalValue());
+
+      Fraction wholeNum = new Fraction(5);
+      Console.WriteLine(wholeNum.GetFractionString());
+      Console.WriteLine(wholeNum.GetDecimalValue());
+
+      Fraction fraction = new Fraction(3, 4);
+      Console.WriteLine(fraction.GetFractionString());
+      Console.WriteLine(fraction.GetDecimalValue());
+
+      Fraction fraction1 = new Fraction(1, 3);
+      Console.WriteLine(fraction1.GetFractionString());
+      Console.WriteLine(fraction1.GetDecimalValue());
+
+   }
 }
