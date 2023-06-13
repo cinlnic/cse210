9c2b77ece5e529d5338dd4b62fd82a1f47871bd3
Cindi Nichols <cindi.l.nichols@gmail.com>
2023-06-13 12:50:27 -0600

    Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
-    }
+   static void Main(string[] args)
+   {
+      List<Shape> shapes = new List<Shape>();
+
+      Square square = new Square("red", 5);
+      shapes.Add(square);
+
+      Rectangle rect = new Rectangle("blue", 8, 4);
+      shapes.Add(rect);
+
+      Circle circle = new Circle("purple", 5);
+      shapes.Add(circle);
+
+
+      foreach (Shape shape in shapes)
+      {
+         Console.WriteLine($"Color: {shape.GetColor()} Area: {shape.GetArea()}");
+      }
+
+
+   }
 }
